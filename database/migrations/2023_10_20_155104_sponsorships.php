af87ab049b7b7302acd5afb024158435f6505c48
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-10-20 18:02:22 +0200

    table sponsorship

---
+++ database/migrations/2023_10_20_155104_sponsorships.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsorships', function (Blueprint $table) {
+            $table->id('sponsorship_id');
+
+            $table->string('name', 40)->nullable(false);
+            $table->decimal('price', 11, 8)->nullable(false);
+            $table->time('duration')->nullable(false);
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsorships');
+    }
+};
